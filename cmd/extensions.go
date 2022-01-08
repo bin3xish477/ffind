50efae0b3e7d1e2b1c791e046c6069e0377fc490
Alexis Rodriguez <44281620+bin3xish477@users.noreply.github.com>
2022-01-08 16:43:02 -0500

    Create extensions.go

---
+++ cmd/extensions.go
@@ -0,0 +1,7 @@
+package cmd
+
+var fileExtentions = []string{
+  ".txt", ".env", ".conf", ".kdb", ".kdbx", ".ini",
+  ".inc", ".php", ".py", ".js", ".json", ".properties",
+  ".pem", ".login", ".yaml", ".yml", ".csv", ".xml",
+}
