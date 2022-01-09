8417a2aef79b9363ebad6ded7235e30f5dc25295
Alexis Rodriguez <rodriguez10011999@gmail.com>
2022-01-09 10:11:29 -0500

    adding main logic to main.go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/bin3xish477/ffind
+
+go 1.17
+
+require (
+	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/alexflint/go-scalar v1.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,60 @@
 package main
 
 import (
-  "github.com/bin3xish477/ffind/cmd"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/alexflint/go-arg"
 )
 
+var args struct {
+	Path string `arg:"-p,--path" default:"." help:"the directory to search"`
+}
+
+func byteCountFromDecimal(b int64) string {
+	const unit = 1000
+	if b < unit {
+		return fmt.Sprintf("%d B", b)
+	}
+	div, exp := int64(unit), 0
+	for n := b / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
+}
+
+func findMatch(path string, f os.FileInfo, err error) error {
+	for ext, description := range fileExtentions {
+		if strings.HasSuffix(path, ext) {
+			fmt.Printf("[%s%sM%s] %s%spath%s: %s, %s%stype%s: %s, %s%ssize%s: %s\n",
+				yellow, bold, end, red, bold, end, path,
+				green, bold, end, description,
+				blue, bold, end, byteCountFromDecimal(f.Size()))
+		}
+	}
+
+	return nil
+}
+
+func walk(path string) {
+	if err := filepath.Walk(path, findMatch); err != nil {
+		fmt.Printf("%serror%s: %s\n", red, end, err.Error())
+	}
+}
+
 func main() {
-  cmd.Execute()
+	arg.MustParse(&args)
+
+	now := time.Now()
+
+	path := filepath.Clean(args.Path)
+
+	walk(path)
+
+	end := time.Since(now)
+	fmt.Printf("\nTime elapsed: %s\n", end)
 }
